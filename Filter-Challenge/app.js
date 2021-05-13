e64d2909ed4b75fa6df0c2240e32b1b94ecf83dc
Matheus <matheusdev20@gmail.com>
2021-05-12 23:16:57 -0300

    Filter Challenge

--- Filter-Challenge/app.js
+++ Filter-Challenge/app.js
@@ -4,24 +4,21 @@ const resetButton = document.getElementById('btn-reset')
 
 
 
-console.log(storeColors);
-console.log(buttons);
-console.log(resetButton);
 
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-        event.preventDefault();
-       const filtered =  event.target.dataset.filter
-       console.log(filtered);
-       storeColors.forEach((color) => {
-           if(color.classList.contains(filtered)){
-               color.style.display = 'block';
-           }
-           else {
-               color.style.display = 'none';
-           }
+        event.preventDefault()
+        console.log(event.target)
+        const filtered = event.target.dataset.filter
+        storeColors.forEach((color) => {
+            if (color.classList.contains(filtered)) {
+                color.style.display = 'block';
+            }
+            else {
+                color.style.display = 'none';
+            }
 
-       })
+        })
     })
 })
 resetButton.addEventListener('click', (e) => {
